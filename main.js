3f41453e5d7a47df537a2ed03eff6f4705c86180
Scripter <gamingkatana611@gmail.com>
2025-12-29 18:19:17 +0100

    Remove unnecessary comment in main.js
    first day on JS

---
+++ main.js
@@ -0,0 +1,22 @@
+public class Main {
+
+    public static void main(String[] args){
+        //↑ the static up there you need to write it everytime to make your code runnable (I think)
+        //(System.out.print) needed to print stuff there is also a shortcut for it if you type "sout" then hit tab!
+        //ln or \n just so when you print multiple phrases, and you don't want them to stick on the same line
+        //←this is a single line comment
+        /*
+        THIS
+        IS
+        MULTI LINE
+        COMMENT
+         */
+
+        IO.println("i love pizza!");
+        IO.println("It's really tasty!");
+        IO.print("i like cats too\n");
+
+
+    }
+}
+
